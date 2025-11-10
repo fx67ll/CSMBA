f425ef35f04f373cd82c651032a5116b63c204cd
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2025-11-10 19:56:56 +0800

    new version record

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 应用全局配置
 module.exports = {
-  baseUrl: 'https://vip.fx67ll.com/vip-api',
-  // baseUrl: 'http://localhost:9897/vip-api',
+  // baseUrl: 'https://vip.fx67ll.com/vip-api',
+  baseUrl: 'http://localhost:9897/vip-api',
   // 不校验登录信息的api
   white_api: ['/captchaImage', '/login', '/register', '/secret/key/getSecretKeyConfigForApp'],
   // 应用信息
@@ -9,7 +9,7 @@ module.exports = {
     // 应用名称
     name: 'csmba-app',
     // 应用版本
-    version: '0.1.2',
+    version: '0.1.3',
     // 应用logo
     logo: 'https://test.fx67ll.com/fx67ll-img-collection/fx67ll.jpg',
     // 个人主页
